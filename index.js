b3fce0d4ad9da16073243263f98593c5f2aaa8c8
afrithbluesprucecapital <afrith@bluesprucecapital.com>
2017-10-09 08:58:14 -0600

    Initial check-in of converted JS

--- index.js
+++ index.js
@@ -1,72 +1,66 @@
-import { _ } from 'lodash';
+'use strict';
 
-const winston = require('winston');
-const fs = require('fs');
+var _lodash = require('lodash');
 
-const LOG_NAME = process.env.LOG_NAME || 'bsc-svc.log';
-const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
-const LOG_LEVEL_CONSOLE = process.env.LOG_LEVEL_CONSOLE || 'verbose';
-const LOG_DIR = '/svc/logs';
+var winston = require('winston');
+var fs = require('fs');
+
+var LOG_NAME = process.env.LOG_NAME || 'bsc-svc.log';
+var LOG_LEVEL = process.env.LOG_LEVEL || 'info';
+var LOG_LEVEL_CONSOLE = process.env.LOG_LEVEL_CONSOLE || 'verbose';
+var LOG_DIR = '/svc/logs';
 
 // Create the log directory if it does not exist
 if (!fs.existsSync(LOG_DIR)) {
   fs.mkdirSync(LOG_DIR);
 }
-const tsFormat = () => (new Date()).toLocaleTimeString();
-const LOGGER = new (winston.Logger)({
+var tsFormat = function tsFormat() {
+  return new Date().toLocaleTimeString();
+};
+var LOGGER = new winston.Logger({
   transports: [
     // colorize the output to the console
-    new (winston.transports.Console)({
+    new winston.transports.Console({
       timestamp: tsFormat,
       colorize: true,
       level: LOG_LEVEL_CONSOLE,
-      showLevel: false,
-    }),
-    new (winston.transports.File)({
-      filename: `${LOG_DIR}/${LOG_NAME}`,
+      showLevel: false
+    }), new winston.transports.File({
+      filename: LOG_DIR + '/' + LOG_NAME,
       timestamp: tsFormat,
       level: LOG_LEVEL,
       json: false,
-      showLevel: false,
-    }),
-  ],
+      showLevel: false
+    })]
 });
 
-const log = (level, title, params, subTitle) => {
-  const rect = (level === 'error') ? '\x1b[41m   \x1b[0m' : '\x1b[42m   \x1b[0m';
+var log = function log(level, title, params, subTitle) {
+  var rect = level === 'error' ? '\x1b[41m   \x1b[0m' : '\x1b[42m   \x1b[0m';
 
   if (level === 'info') {
     LOGGER[level](title);
   } else if (level === 'verbose') {
     if (subTitle) {
-      LOGGER[level](
-        `${rect}  ${title} ---- ${subTitle}\n`,
-      );
+      LOGGER[level](rect + '  ' + title + ' ---- ' + subTitle + '\n');
     } else {
-      LOGGER[level](
-        `${rect}  ${title}\n`,
-      );
+      LOGGER[level](rect + '  ' + title + '\n');
     }
   } else {
-    let pStr = '\n';
-    _.each(params, (obj) => {
-      _.each(obj, (value, key) => {
-        pStr += `\n${key} \x1b[2m::: ${value}\x1b[0m`;
+    var pStr = '\n';
+    _lodash._.each(params, function (obj) {
+      _lodash._.each(obj, function (value, key) {
+        pStr += '\n' + key + ' \x1B[2m::: ' + value + '\x1B[0m';
       });
     });
 
     if (subTitle) {
-      LOGGER[level](
-        `\n\n${rect} ${title} ---------- ${subTitle} ---------- ${pStr}\n`,
-      );
+      LOGGER[level]('\n\n' + rect + ' ' + title + ' ---------- ' + subTitle + ' ---------- ' + pStr + '\n');
     } else {
-      LOGGER[level](
-        `\n\n${rect}  ${title} ---------- ${pStr}\n`,
-      );
+      LOGGER[level]('\n\n' + rect + '  ' + title + ' ---------- ' + pStr + '\n');
     }
   }
 };
 
 module.exports = {
-  LOGGER, log,
+  LOGGER: LOGGER, log: log
 };
